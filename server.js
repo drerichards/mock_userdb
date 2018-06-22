090ad43a61759f0eec55769da7ecc47a77916179
Andre Richardson <g.andrerichards@gmail.com>
2018-06-22 05:30:33 -0400

    fetch api data fromdb with route

---
+++ server.js
@@ -0,0 +1,33 @@
+"use strict"
+require("dotenv").config()
+const express = require("express"),
+    app = express(),
+    cors = require("cors"),
+    bodyParser = require("body-parser"),
+    axios = require('axios'),
+    {
+        DATABASE_URL,
+        PORT
+    } = require("./config")
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get("/users", (req, res) => {
+    try {
+        axios.get(DATABASE_URL)
+            .then(response => {
+                console.log(response.data[0].users)
+            }).catch(error => {
+                res.status(500).json({
+                    message: error
+                })
+            })
+    } catch (error) {
+        res.status(500).json({
+            message: error
+        })
+    }
+})
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
